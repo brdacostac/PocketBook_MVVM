e48ae6104eadece389160ffed5d8a78dae1eaf39
Bruno Da Costa Cunha <Bruno.DA_COSTA_CUNHA@etu.uca.fr>
2023-09-04 22:42:19 +0200

    Ajout des fichiers

---
+++ TP_MVVM/TP_MVVM/App.xaml.cs
@@ -0,0 +1,12 @@
+﻿namespace TP_MVVM
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new AppShell();
+        }
+    }
+}
