5442aeafc853e94791b5863ed31476ecd229e298
Bruno Da Costa Cunha <Bruno.DA_COSTA_CUNHA@etu.uca.fr>
2023-09-10 18:02:04 +0200

    Grille avec menu ajouté

--- TP_MVVM/TP_MVVM/View/MainPage.xaml.cs
+++ TP_MVVM/TP_MVVM/View/MainPage.xaml.cs
@@ -7,6 +7,20 @@
         public MainPage()
         {
             InitializeComponent();
+
+
+            var items = new List<object>
+        {
+            new { IconPath = "Resources/Images/tray_fill.png", Text = "Tous", CountBook = 45 },
+            new { IconPath = "Resources/Images/person_badge_clock_fill.png", Text = "En prêt", CountBook = 1 },
+            new { IconPath = "Resources/Images/arrow_forward.png", Text = "À lire plus tard", CountBook = "" },
+            new { IconPath = "Resources/Images/eyeglasses.png", Text = "Statut de lecture", CountBook = "" },
+            new { IconPath = "Resources/Images/heart_fill.png", Text = "Favoris", CountBook = "" },
+            new { IconPath = "Resources/Images/tag_fill.png", Text = "Étiquettes", CountBook = "" }
+        };
+
+            
+            listView.ItemsSource = items;
         }
 
     }
