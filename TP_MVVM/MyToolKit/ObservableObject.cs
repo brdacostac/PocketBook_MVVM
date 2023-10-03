9a364f7f2a06e3ffbaa500165801d09ed753d08f
étudiant <etudiant@MB-R-110-001-178.local>
2023-10-03 12:01:25 +0200

    Avancé dans le relay command et observableObject

---
+++ TP_MVVM/MyToolKit/ObservableObject.cs
@@ -0,0 +1,23 @@
+﻿
+
+using System.ComponentModel;
+using System.Runtime.CompilerServices;
+
+namespace MyToolKit;
+
+
+public class ObservableObject : INotifyPropertyChanged
+{
+    public event PropertyChangedEventHandler? PropertyChanged;
+    protected virtual void OnPropertyChanged([CallerMemberName] string property = null) =>
+        PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(property));
+
+    protected virtual void SetProperty<T>(T member, T value, Action<T> action, [CallerMemberName] string property = null)
+    {
+        if (EqualityComparer<T>.Default.Equals(member, value))
+            return;
+        action(value);
+        OnPropertyChanged(property);
+    }
+}
+
---
+++ TP_MVVM/MyToolKit/RelayCommand.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Windows.Input;
+
+namespace MyToolKit
+{
+
+    public class RelayCommand<T> : ICommand
+	{
+
+
+        private readonly Action<T> execute;
+
+        private readonly Func<T, bool> canExecute;
+
+
+        public RelayCommand(Action<T> execute, Func<T, bool> canExecute = null)
+        {
+            this.execute = execute;
+            this.canExecute = canExecute;
+        }
+
+
+        public event EventHandler? CanExecutedChanged;
+
+        public void Exeute(object? parameter)
+        {
+            T param = (T)parameter;
+            execute(param);
+        }
+
+        public bool CanExecute(object? parameter)
+        {
+            T param = (T)parameter;
+            return CanExecute(param);
+        }
+
+        public void RefreshCommand() => CanExecutedChanged?.Invoke(this, EventArgs.Empty);
+
+
+
+        event EventHandler ICommand.CanExecuteChanged
+        {
+            add
+            {
+                throw new NotImplementedException();
+            }
+
+            remove
+            {
+                throw new NotImplementedException();
+            }
+        }
+
+
+        bool ICommand.CanExecute(object parameter)
+        {
+            throw new NotImplementedException();
+        }
+
+        void ICommand.Execute(object parameter)
+        {
+            throw new NotImplementedException();
+        }
+
+        
+    }
+
+    public class RelayCommand : RelayCommand<object>
+    {
+        //Faut recopier cela:
+        public RelayCommand(Action<object> execute, Func<object, bool> canExecute = null) : base(execute, canExecute)
+        {
+        }
+    }
+}
+
